769eb200e6d5edeb3d285bcfb96d96a11a6c4600
Ting Chen <sally20050203@gmail.com>
2017-10-08 22:29:36 +1100

    add single ticket view

--- app/app.js
+++ app/app.js
@@ -7,7 +7,7 @@ angular.module('myApp', [
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({redirectTo: '/'});
 }])
 .controller("ticketCtrl", function($scope, $http) {
 
@@ -16,8 +16,11 @@ config(['$locationProvider', '$routeProvider', function($locationProvider, $rout
     $scope.previous = "";
     $scope.next = "";
     $scope.errorMsg = null;
+    $scope.singleView = false;
+    $scope.ticket = null;
 
     function fetchTickets(page) {
+        $scope.singleView = false;
         var req = {
             method: 'GET',
             url: 'http://localhost:9000/tickets'+page+'&jsonType=true',
@@ -32,6 +35,29 @@ config(['$locationProvider', '$routeProvider', function($locationProvider, $rout
                  $scope.previous = response.data.previous ? response.data.previous : "";
                  $scope.next = response.data.next ? response.data.next : "";
                  $scope.errorMsg = null;
+             } else {
+                $scope.errorMsg = "Opps, something wrong in fetching tickets";
+             }
+         }, function(response) {
+             $scope.errorMsg = response.statusText;
+             $scope.errorMsg += (response.data && response.data.message) ? response.data.message : "";
+             $scope.errorMsg = ($scope.errorMsg == "") ? "Opps, something wrong in fetching tickets" : $scope.errorMsg; 
+        });
+    };
+
+    $scope.selectSingleTicket = function(id) {
+        $scope.singleView = true;
+        var req = {
+            method: 'GET',
+            url: 'http://localhost:9000/tickets/'+id+'?jsonType=true',
+            headers: {
+              'Content-Type': "application/json",
+            }
+         }
+         $http(req).then(function(response) {
+             if (response.statusText == "OK") {
+                 $scope.errorMsg = null;
+                 $scope.ticket = response.data.ticket;
              } else {
                 $scope.errorMsg = "Opps, something wrong in fetching tickets";
              }
